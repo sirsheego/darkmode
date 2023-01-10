201021d9888b489900e2ff5b56fb55ccf388beff
sirsheego <sirsheego@gmail.com>
2023-01-10 14:30:43 +0100

    darktoggle

---
+++ index.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "blue";
+  } else {
+    bodyEl.style.background = "yellow";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
